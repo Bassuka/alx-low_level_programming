266a3bea44a960c54b62a773b837032271e2fd0d
Basselael <kassabasselael@gmail.com>
2023-07-14 20:57:07 -0700

    print small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description - prints alphabet in lowercase
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a < 122)
+	{
+	putchar (a);
+	a++;
+	}
+	return (0);
+}
