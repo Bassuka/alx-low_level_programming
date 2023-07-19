454ce8964e3bbb0b1b9d065d248f2f09d40dbde6
Basselael <kassabasselael@gmail.com>
2023-07-19 12:45:36 -0700

    print lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Descritption - lowercase using new header
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (i = 97; i < = 122; i++)
+	{
+	_putchar('i');
+	}
+	_putchar('\n');
+}
