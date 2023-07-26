7ab18c4f793d6d6eaa6a3530fef1975b05491342
Basselael <kassabasselael@gmail.com>
2023-07-26 13:58:46 -0700

    stract

---
+++ 0x06-pointers_arrays_strings/0-stract.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_stract : the src string to the dest string
+ * Description - overwriting the terminating null byte (\0) at the end of dest
+ *  and then adds a terminating null byte
+ *
+ * @a : the pointer to dest
+ * @b : the pointer to src
+ * Return : a pointer resulting string dest
+ */
+char *_stract(char *a, char *b)
+{
+	return (*a);
+}
